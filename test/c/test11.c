9a6e4c0cd81c2c620dab51cce766c85d449625e3
Steve Margerm <smargerm@sfu.ca>
2018-01-29 14:38:43 -0800

    Renaming test11 and test12.

--- test/c/test11.c
+++ test/c/test11.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
-unsigned test11(unsigned j) {
-  unsigned foo = j;
+unsigned test11_add(unsigned a, unsigned b) {
+  unsigned sum;
+  sum = a + b;
+  return sum;
+}
+
+unsigned test11(unsigned *a, unsigned *b, unsigned *c) {
   for (unsigned i = 0; i < 5; ++i) {
-      for(unsigned k = 0; k < 5; ++k){
-        foo++;
-      }
+    c[i] = test11_add(a[i],b[i]);
   }
-  return foo;
+  return 1;
 }
 
 int main() {
-  int result = test11(100);
-  printf("%d\n",result);
+  int i;
+  unsigned a[5] = {1,2,3,4,5};
+  unsigned b[5] = {1,2,3,4,5};
+  unsigned c[5] = {0};
+  test11(a,b,c);
+  for(i=0;i<5;i++) {
+    printf("%d\n", c[i]);
+  }
+
 }
