3747ef24a29f7f18cc7c9af43a1e8fd0b8b40d5e
Steve Margerm <smargerm@sfu.ca>
2018-02-26 17:16:01 -0800

    Changed test04.c to a while loop.

--- test/c/test04.c
+++ test/c/test04.c
@@ -1,21 +1,19 @@
-#define COL_SIZE 10
-#define ROW_SIZE 10
+#include <stdio.h>
 
-int foo(unsigned int a, unsigned int b, unsigned int n){
-    unsigned int sum = a;
-    unsigned i,j;
-
-    for(i = 0 ; i < COL_SIZE; i++){
-        for(j = 0; j < ROW_SIZE; j++){
-            sum = (sum+a)*b;
-        }
-    }
-    return sum;
+void test04(int a, int b){
+ Minim_Loop: while (a != b) {
+    if (a > b)
+      a -= b;
+    else
+      b -= a;
+    printf("a=%d, b=%d\n",a,b);
+  } 
+  return;
 }
 
 int main(){
-    unsigned int a = 5;
-    unsigned int b = 3;
-    unsigned int sum = foo(a,b, 2);
+    unsigned int a = 50;
+    unsigned int b = 5;
+    test04(a,b);
     return 0;
 }
