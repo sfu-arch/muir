f7e791ecf8fa7dfdb22d3daf3846b439e09ff394
Steve Margerm <smargerm@sfu.ca>
2018-03-14 11:28:55 -0700

    Added cilk_for_test05.c, cilk_for_test06.c which contain more complicated and nested pfor loops respectively.

---
+++ test/c/cilk/cilk_for_test06.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <cilk/cilk.h>
+
+int cilk_for_test06(int a[][5], int b[][5], int c[][5]) {
+  cilk_for (int i = 0; i < 5; ++i) {
+    cilk_for (int j = 0; j < 5; ++j) {
+      c[i][j]=a[i][j]+b[i][j];
+    }
+  }
+
+  return 1;
+}
+
+int main() {
+  int i,j;
+  int a[5][5] = {{ 1, 2, 3, 4, 5},
+		 {11,12,13,14,15},
+		 {21,22,23,24,25},
+		 {31,32,33,34,35},
+		 {41,42,43,44,45}};
+  int b[5][5] = {{ 1, 2, 3, 4, 5},
+		 {11,12,13,14,15},
+		 {21,22,23,24,25},
+		 {31,32,33,34,35},
+		 {41,42,43,44,45}};
+  int c[5][5] = {0};
+  cilk_for_test06(a,b,c);
+  for(i=0;i<5;i++) {
+    for(j=0;j<5;j++) {
+      printf("%d\n", c[i][j]);
+    }
+  }
+
+}
