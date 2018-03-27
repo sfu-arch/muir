6a2a86002d154c427a69428ae6f77258f1ddf0db
Steve Margerm <smargerm@sfu.ca>
2018-03-27 15:21:12 -0700

    Updated Makefile to split out detach blocks usign Apala's branch.

--- test/c/cilk/stencil.c
+++ test/c/cilk/stencil.c
@@ -3,16 +3,16 @@
 #include <stdlib.h>
 
 #define NROWS (4)
-#define NCOLS (4)
+#define NCOLS (4)  // NCOLS must be power of 2 to avoid use of MOD operator (unsupported)
 #define NBRROWS (1)
 #define NBRCOLS (1)
 #define NWORKERS (4)
 #define NSTEPS (5)
 #define FILTCOLS (1 + NBRROWS * 2)
 #define FILTROWS (1 + NBRCOLS * 2)
+//#define UNSIGNED
 
-
-void print (int x[]) {
+void print (unsigned x[]) {
   printf("------------------------------------------------------------------------------------------------\n");
   for (int i = 0; i < NROWS; i ++ ) {
     for (int j = 0; j < NCOLS; j ++) {
@@ -22,17 +22,17 @@ void print (int x[]) {
   }
 }
 
-
-void stencil (int in[], int out[]) {
-  cilk_for (int pos = 0; pos < NROWS * NCOLS; pos ++) {
-    int i = pos / NCOLS;
-    int j = pos % NCOLS;
-    for (int nr = -NBRROWS; nr <= NBRROWS; nr ++) {
-      for (int nc = -NBRCOLS; nc <= NBRCOLS; nc ++) {
-	int row = i + nr;
-	int col = j + nc;
-	if ((row >= 0) && (row < NROWS)) {
-	  if ((col >=0) && (col < NCOLS)) {
+#ifdef UNSIGNED
+void stencil (unsigned in[], unsigned out[]) {
+  cilk_for (unsigned pos = 0; pos < NROWS * NCOLS; pos ++) {
+    unsigned i = pos / NCOLS;
+    unsigned j = pos & (NCOLS-1);
+    for (unsigned nr = 0; nr <= 2*NBRROWS; nr ++) {
+      for (unsigned nc = 0; nc <= 2*NBRCOLS; nc ++) {
+	unsigned row = i + nr - NBRROWS;
+	unsigned col = j + nc - NBRCOLS;
+	if ((row < NROWS)) {
+	  if ((col < NCOLS)) {
 	    out[i * NCOLS + j] += in[row * NCOLS + col];
 	  }
 	}
@@ -42,12 +42,32 @@ void stencil (int in[], int out[]) {
       (FILTROWS * FILTCOLS);
   }
 }
-
+#else
+void stencil (unsigned in[], unsigned out[]) {
+  cilk_for (int pos = 0; pos < NROWS * NCOLS; pos ++) {
+    int i = pos / NCOLS;
+    int j = pos & (NCOLS-1);
+        for (int nr = -NBRROWS; nr <= NBRROWS; nr ++) {
+          for (int nc = -NBRCOLS; nc <= NBRCOLS; nc ++) {
+	    int row = i + nr - NBRROWS;
+	    int col = j + nc - NBRCOLS;
+	    if ((row >= 0) && (row < NROWS)) {
+	      if ((col >=0) && (col < NCOLS)) {
+		out[i * NCOLS + j] += in[row * NCOLS + col];
+	  }
+	}
+      }
+    }
+    out[i * NCOLS + j] = (out[i * NCOLS + j] + (FILTROWS * FILTCOLS)) /
+      (FILTROWS * FILTCOLS);
+  }
+}
+#endif
 
 int main () {
   //initialize data
-  int buf0[NROWS * NCOLS];
-  int buf1[NROWS * NCOLS];
+  unsigned buf0[NROWS * NCOLS];
+  unsigned buf1[NROWS * NCOLS];
   for (int i = 0; i < NROWS; i++) {
     for (int j = 0; j < NCOLS; j ++) {
       buf0[i * NCOLS + j] = rand() % 10;
@@ -59,6 +79,7 @@ int main () {
     if (i % 2) stencil(buf1, buf0);
     else stencil(buf0, buf1);
   }
+  print(buf1);
   return 0;
 }
 
