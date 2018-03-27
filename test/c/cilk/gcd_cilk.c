9e82c43d0fce1c96eb0267ffa2bd13b3035d857e
Steve Margerm <smargerm@sfu.ca>
2018-03-27 07:20:57 -0700

    Added gcd example.

---
+++ test/c/cilk/gcd_cilk.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <cilk/cilk.h>
+
+__attribute__((always_inline))
+inline int gcd(int n1, int n2) {
+  while(n1 != n2)
+    {
+      if(n1 > n2)
+	n1 -= n2;
+      else
+	n2 -= n1;
+    }
+  return n1;
+}
+
+int gcd_cilk(int n[][2], int size) {
+  cilk_for (int i = 0; i < size; ++i) {
+    n[i][0] = gcd(n[i][0], n[i][1]);
+  }
+  return 1;
+}
+
+int main() {
+  int n[4][2]= {{10,20},
+		{7,21},
+		{81,27},
+		{24,36}};
+  
+  int result = gcd_cilk(n,4);
+  for (int i=0; i<4;i++) {
+    printf("%d\n",n[i][0]);
+  }
+}
+
